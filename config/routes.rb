394e3b412eed445d169438b73546af8c9f053c98
sonvuhuu18 <son.vuhuu18@gmail.com>
2017-10-02 09:48:51 +0700

    Sign in with Google

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :categories
   root "static_pages#home"
 
-  devise_for :users, controllers: {registrations: "registrations"}
+  devise_for :users, controllers: {registrations: "registrations",
+    omniauth_callbacks: "users/omniauth_callbacks"}
 end
