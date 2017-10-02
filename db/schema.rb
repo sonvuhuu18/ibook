b5e909a60837c7835da64ce63dc0ec3454fe9e75
sonvuhuu18 <son.vuhuu18@gmail.com>
2017-10-02 09:48:51 +0700

    Sign in with Google

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001143733) do
+ActiveRecord::Schema.define(version: 20171002022448) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20171001143733) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
