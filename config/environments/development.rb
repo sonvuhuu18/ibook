49cf8529fac3700416fc7e772fc066e8ba6d4d0d
Cường Nguyễn <cnpgvc@gmail.com>
2017-10-05 11:38:23 +0000

    Add my URL to mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.default_url_options = {host: 'localhost:3000'}
+  config.action_mailer.default_url_options = {host: 'https://masterrace-cuongnguyen19.c9users.io'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
