be6c43304047f38ef8fe5cb8aa3b5a4a13217c4f
sonvuhuu18 <son.vuhuu18@gmail.com>
2017-10-09 20:26:12 +0700

    Fix minor bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {registrations: "registrations",
+    omniauth_callbacks: "users/omniauth_callbacks"}
   resources :reviews, only: :index
-  resources :users
+  resources :users, only: :show
   resources :books do
     resources :reviews, except: :index
   end
   resources :categories
   root "static_pages#home"
-
-  devise_for :users, controllers: {registrations: "registrations",
-    omniauth_callbacks: "users/omniauth_callbacks"}
 end
