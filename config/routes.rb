81050684880876d8121489f61af8ba75d657a810
sonvuhuu18 <son.vuhuu18@gmail.com>
2017-10-13 14:59:15 +0700

    Admin now can edit users' role

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: "registrations",
     omniauth_callbacks: "users/omniauth_callbacks"}
   resources :reviews, only: :index
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit, :update]
   resources :books do
     resources :reviews, except: :index
     member do
