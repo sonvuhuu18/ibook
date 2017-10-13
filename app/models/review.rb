1efbeb4977928fd917f6d8e2e7120d2a04727978
sonvuhuu18 <son.vuhuu18@gmail.com>
2017-10-13 09:01:35 +0700

    Validate review rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,11 @@ class Review < ApplicationRecord
   validates :content, presence: true,
     length: {minimum: 6, maximum: 1000}
   validates :rating, presence: true
+  validate :rating_between_1_and_5
+
+  private
+
+  def rating_between_1_and_5
+    errors.add(:base, "Please choose your rating") unless (1..5).include?(rating)
+  end
 end
