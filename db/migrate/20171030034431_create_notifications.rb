5dd9f775cfca8866a80f39573b01c81ea685eefd
sonvuhuu18 <son.vuhuu18@gmail.com>
2017-11-27 23:08:11 +0700

    Fix bug delete user

--- db/migrate/20171030034431_create_notifications.rb
+++ db/migrate/20171030034431_create_notifications.rb
@@ -1,8 +1,8 @@
 class CreateNotifications < ActiveRecord::Migration[5.1]
   def change
     create_table :notifications do |t|
-      t.integer :user_id
-      t.integer :notified_by_id
+      t.references :user, index: true
+      t.references :notified_by, index: true
       t.string :notice_type
       t.string :book_id
       t.string :request_status
@@ -11,7 +11,7 @@ class CreateNotifications < ActiveRecord::Migration[5.1]
 
       t.timestamps
     end
-
-    add_index :notifications, :user_id
+    add_foreign_key :notifications, :users
+    add_foreign_key :notifications, :users, column: :notified_by_id
   end
 end
